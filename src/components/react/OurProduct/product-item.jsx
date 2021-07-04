d4b6a01015ddb04cf4e50cf20bd4ceb4e9a37505
Tinh Ha <tinh.ha@officeatwork.com>
2021-07-04 21:47:03 +0700

    update product

--- src/components/react/OurProduct/product-item.jsx
+++ src/components/react/OurProduct/product-item.jsx
@@ -13,6 +13,9 @@ import {
 } from "@material-ui/core";
 import { SelectMenuContext } from "../TopMenu/select-menu-context-provider";
 import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -44,9 +47,10 @@ const useStyles = makeStyles((theme) => ({
         left: "15%",
         backgroundColor: "#ffffff",
         display: "flex",
-        padding: 20,
+        padding: 30,
         flexDirection: 'row',
         justifyContent: 'space-between',
+        position: 'relative',
     },
     modalClose: {
         position: 'absolute',
@@ -57,7 +61,24 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: 'rgb(51, 153, 255)',
         borderRadius: '50%',
         cursor: 'pointer',
-    }
+    },
+    modalNavigation: {
+        position: 'absolute',
+        top: 0,
+        height: '100%',
+        width: 50,
+        display: 'flex',
+        alignItems: 'center',
+    },
+    modalPrev: {
+        left: 0,
+    },
+    modalNext: {
+        right: 0,
+    },
+    modalButton: {
+        height: 50,
+    },
 }));
 
 export const ProductItem = () => {
@@ -123,6 +144,28 @@ const ProductItemMainInfo = ({ productLine, productList }) => {
 
     const handleCloseModal = () => setOpenModal(false);
 
+    const goNext = () => {
+        const itemIndex = productList.findIndex(i => i.imageUrl === selectedItem.imageUrl);
+
+        if (itemIndex >= productList.length - 1) {
+            setSelectedItem(productList[0]);
+            return;
+        }
+
+        setSelectedItem(productList[itemIndex + 1]);
+    }
+
+    const goNPrev = () => {
+        const itemIndex = productList.findIndex(i => i.imageUrl === selectedItem.imageUrl);
+
+        if (itemIndex === 0) {
+            setSelectedItem(productList[productList.length - 1]);
+            return;
+        }
+
+        setSelectedItem(productList[itemIndex - 1]);
+    }
+
     return (
         <div className="gb-padding">
             <Grid container spacing={3}>
@@ -138,6 +181,16 @@ const ProductItemMainInfo = ({ productLine, productList }) => {
                         <img src={selectedItem.imageUrl} />
                     </div>
                     <div onClick={handleCloseModal} className={style.modalClose}><CloseIcon style={{ fontSize: 40, color: '#ffffff' }} /></div>
+                    <div className={`${style.modalNavigation} ${style.modalPrev}`}>
+                        <IconButton onClick={goNPrev} className={style.modalButton}>
+                            <ArrowBackIosIcon />
+                        </IconButton>
+                    </div>
+                    <div className={`${style.modalNavigation} ${style.modalNext}`}>
+                        <IconButton onClick={goNext} className={style.modalButton}>
+                            <ArrowForwardIosIcon />
+                        </IconButton>
+                    </div>
                 </div>
             </Modal>
         </div>
